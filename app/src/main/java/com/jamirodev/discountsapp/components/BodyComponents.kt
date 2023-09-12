4baa55c26afa88e470ea16dbee4eb357956d5389
Jamiro <jamirocristian15@gmail.com>
2023-09-12 16:03:53 -0300

    Added spacers in homeview.

--- app/src/main/java/com/jamirodev/discountsapp/components/BodyComponents.kt
+++ app/src/main/java/com/jamirodev/discountsapp/components/BodyComponents.kt
@@ -1,4 +1,19 @@
 package com.jamirodev.discountsapp.components
 
-class BodyComponents {
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpaceH(size: Dp = 5.dp) {
+    Spacer(modifier = Modifier.height(size))
+}
+
+
+@Composable
+fun SpaceW(size: Dp = 5.dp) {
+    Spacer(modifier = Modifier.height(size))
 }
--- app/src/main/java/com/jamirodev/discountsapp/views/HomeView.kt
+++ app/src/main/java/com/jamirodev/discountsapp/views/HomeView.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.jamirodev.discountsapp.components.SpaceH
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
